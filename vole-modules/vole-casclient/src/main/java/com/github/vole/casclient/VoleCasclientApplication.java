eb2ccda64d79a4d35a988f1c1d75e257669a2a26
Scottglenn <chensir521@hotmail.com>
2020-04-18 21:34:05 +0800

    修改配置

--- vole-modules/vole-casclient/src/main/java/com/github/vole/casclient/VoleCasclientApplication.java
+++ vole-modules/vole-casclient/src/main/java/com/github/vole/casclient/VoleCasclientApplication.java
@@ -1,14 +1,20 @@
 package com.github.vole.casclient;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@Slf4j
 @EnableDiscoveryClient
 @SpringBootApplication
 public class VoleCasclientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VoleCasclientApplication.class, args);
+        log.debug("VoleCasclientApplication startup main");
+        SpringApplication application = new SpringApplication(VoleCasclientApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
     }
 }
